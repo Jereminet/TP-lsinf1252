ffcec6464a04e568d88c580590fdf4cbc1549450
Jeremy Minet <jeremy.minet@student.uclouvain.be>
2017-03-15 15:34:23 +0100

    ajout fichiers tp6

---
+++ tp6_1.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+int global=0;
+static int static_global=0;
+
+void * fun(void *arg){
+	global++;
+	static_global++;
+	foo(); //+return
+}
+
+void foo(void){
+	int local=0;
+	int static_local=0; //valeur conservee entre les appels de fonctions, stockee dans la memoire "gloabl"
+	static_local++; //+return
+}
+
+pthread_t thread;
+
+int main (int argc, char *argv[]){
+	int var;
+	pthread_create(&thread,NULL,&fun,&var);
+	foo();
+	global++;
+        static_global++;
+	var++;
+	pthread_join(thread,NULL);	
+}
